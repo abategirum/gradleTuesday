01e0183f2076a9196567c69b851de769cf087d44
Girum Feleke <girumabate22@gmail.com>
2024-05-28 11:26:50 -0500

    intitial commit

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.run{
+    register<JavaExec>("run") {
+        mainClass = "miu.edu.cse.Main"
+        classpath = sourceSets.main.get().runtimeClasspath
+    }
+}
+
